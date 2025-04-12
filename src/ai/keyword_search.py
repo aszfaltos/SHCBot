a63c5cb77ed236d82bad7aec5a937d5b79d9c363
domirick <domirick@pm.me>
2025-04-12 23:23:02 +0200

    VectorDB refactorings - Merged VectorDB and RetrieverHandler classes to VectorDatabase - Renamed KeywordSearchRetriever to KeywordRetriever - Made Reranker and KeywordRetriever classes static - Updated debug messages - Updated sample .env

--- src/ai/keyword_search.py
+++ src/ai/keyword_search.py
@@ -2,28 +2,22 @@ from langchain_community.retrievers import BM25Retriever
 from langchain.docstore.document import Document
 from nltk.tokenize import word_tokenize
 
-class KeywordSearchRetriever:
+class KeywordRetriever:
     """
     A class to perform keyword search on a list of documents.
     """
-    def __init__(self, chunks: list[Document], top_k: int = 5):
+    def get_retriever(chunks: list[Document], top_k: int = 5) -> BM25Retriever:
         """
         Initialize a retriever with document chunks and the number of top results to retrieve.
         Args:
             chunks (list[Document]): A list of document chunks to search through.
             top_k (int): The number of top results to retrieve. Defaults to 5.
+        Returns:
+            BM25Retriever: The keyword retriever.
         """
-
-        self.keyword_retriever = BM25Retriever.from_documents(
+        keyword_retriever = BM25Retriever.from_documents(
             chunks,
             k=top_k,
             preprocess_func=word_tokenize,
         )
-    
-    def get_retriever(self) -> BM25Retriever:
-        """
-        Get the keyword retriever.
-        Returns:
-            BM25Retriever: The keyword retriever.
-        """
-        return self.keyword_retriever
+        return keyword_retriever
--- src/ai/reranker.py
+++ src/ai/reranker.py
@@ -3,25 +3,20 @@ from langchain.retrievers.document_compressors import CrossEncoderReranker
 from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 
 class Reranker:
-    def __init__(self, model_name: str, retriever, top_k: int = 5):
+    def get_retriever(model_name: str, retriever, top_k: int = 5) -> ContextualCompressionRetriever:
         """
-        Initialize the Reranker class.
+        Creates a reranker retriever.
         Args:
             model_name (str): The name of the model to use for reranking from HuggingFaceCrossEncoder.
             retriever: The base retriever to use for initial retrieval.
             top_k (int): The number of top results to retrieve after reranking. Defaults to 5.
-        """
-        model = HuggingFaceCrossEncoder(model_name=model_name)
-        compressor = CrossEncoderReranker(model=model, top_n=top_k)
-        self.compression_retriever = ContextualCompressionRetriever(
-            base_compressor=compressor, 
-            base_retriever=retriever
-        )
-    
-    def get_retriever(self) -> ContextualCompressionRetriever:
-        """
-        Get the retriever.
         Returns:
             ContextualCompressionRetriever: The compression retriever.
         """
-        return self.compression_retriever
+        model = HuggingFaceCrossEncoder(model_name=model_name)
+        compressor = CrossEncoderReranker(model=model, top_n=top_k)
+        compression_retriever = ContextualCompressionRetriever(
+            base_compressor=compressor, 
+            base_retriever=retriever
+        )
+        return compression_retriever
